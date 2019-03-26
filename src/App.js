d25b6d4cadad99d117807c5cad2864c16995a35f
David <belmontecast@gmail.com>
2019-03-26 13:02:01 +0100

    hum des donuts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Quote from "./Quote";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: props.on
+    };
+  }
+  handleClick = () => {
+    this.setState({ on: !this.state.on });
+  };  
   render() {
+    const working = this.state.on ? 'work' : 'pause';
+    const logotourne = this.state.on ? "App-logo" : "App-logo2";
     return (
-      <div className="App">
+      <div className="App">       
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className={logotourne} alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
         </header>
+        <button
+  onClick={this.handleClick}
+  className={working}
+>
+  {working.toUpperCase()}
+</button>
+        <figure className={working} />
+        <Quote
+          quote="I believe the children are the future... Unless we stop them now!"
+          character="Homer Simpson"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+        />
+        <Quote
+          quote="Me fail English? That's unpossible"
+          character="Ralph Wiggum"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
+        />
       </div>
     );
   }
